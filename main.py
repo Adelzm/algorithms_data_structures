412f603cae57036c1ea5c4b3419c5022e0852479
Adel Muhandes <adel.muhandes@liebherr.com>
2025-04-29 10:55:46 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Main entry point for testing algorithms and data structures implementations.
+"""
+
+def main():
+    """
+    Main function to test your algorithms and data structures.
+    Add your test code here or import and use functions from other modules.
+    """
+    print("Welcome to Algorithms and Data Structures Practice!")
+    
+    # Example of running a test
+    # Uncomment and modify the following lines as needed:
+    # from data_structures.linked_list import LinkedList
+    # my_list = LinkedList()
+    # my_list.append(1)
+    # my_list.append(2)
+    # my_list.append(3)
+    # print(my_list)
+    
+    # Your test code goes here
+
+if __name__ == "__main__":
+    main()
