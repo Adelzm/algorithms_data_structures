bcaa294607170713827b1bfedb6a0946f7615d6a
Adel Muhandes <adel.muhandes@liebherr.com>
2025-05-12 07:51:19 +0200

    linked list

---
+++ data_structures/linked_list.py
@@ -0,0 +1,84 @@
+class LinkedList:
+  def __init__(self, value):
+    new_node = Node(value)
+    self.head = new_node
+    self.tail = new_node
+    self.length = 1
+
+  def append(self, value):
+    new_node = Node(value)
+    if self.head is None:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      self.tail.next = new_node
+      self.tail = new_node
+    self.length += 1
+
+  def pop(self):
+    if self.length == 0:
+      return None
+    temp = self.head
+    pre = self.head
+    while temp.next is not None:
+      pre = temp
+      temp = temp.next
+    self.tail = pre
+    self.tail.next = None
+    self.length -= 1
+    if self.length == 0:
+      self.head = None
+      self.tail = None
+    return temp
+  
+  def prepend(self, value):
+    new_node = Node(value)
+    if self.length == 0:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      new_node.next = self.head
+      self.head = new_node
+    self.length += 1
+    return True
+
+  def print_list(self):
+    current = self.head
+    while current is not None:
+      print(current)
+      current = current.next
+    pass
+
+  def find_middle_node(self):
+        fast = self.head
+        slow = self.head
+        
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next if fast and fast.next else None
+        
+        return slow
+
+
+class Node:
+  def __init__(self, value):
+    self.value = value
+    self.next = None
+    
+  def __str__(self):
+    next_value = self.next.value if self.next else None
+    return f"Node(value: {self.value}, next: {next_value})"
+
+
+def find_kth_from_end(ll, k):
+    slow = fast = ll.head   
+    for _ in range(k):
+        if fast is None:
+            return None
+        fast = fast.next
+ 
+    while fast:
+        slow = slow.next
+        fast = fast.next
+        
+    return slow
--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-#!/usr/bin/env python3
-"""
-Main entry point for testing algorithms and data structures implementations.
-"""
+from data_structures.linked_list import *
 
 def main():
-    """
-    Main function to test your algorithms and data structures.
-    Add your test code here or import and use functions from other modules.
-    """
-    print("Welcome to Algorithms and Data Structures Practice!")
-    
-    # Example of running a test
-    # Uncomment and modify the following lines as needed:
-    # from data_structures.linked_list import LinkedList
-    # my_list = LinkedList()
-    # my_list.append(1)
-    # my_list.append(2)
-    # my_list.append(3)
-    # print(my_list)
-    
-    # Your test code goes here
+    my_linked_list = LinkedList(1)
+    my_linked_list.append(2)
+    my_linked_list.append(3)
+    my_linked_list.append(4)
+    my_linked_list.append(5)
+
+
+    k = 1
+    result = find_kth_from_end(my_linked_list, k)
+
+    print(result.value)  # Output: 4
+
 
 if __name__ == "__main__":
     main()
+
