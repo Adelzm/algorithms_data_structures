3458ba3363cf6e39d9267bb3409dfff5af0f1345
Adel Muhandes <adel.muhandes@liebherr.com>
2025-05-14 22:14:53 +0200

    implement doubly linked list

--- data_structures/doubly_linked_list.py
+++ data_structures/doubly_linked_list.py
@@ -34,5 +34,125 @@ class DoublyLinkedList:
       new_node.prev = self.tail
       self.tail = new_node
     self.length += 1
+    return True
+
+  def pop(self):
+    if self.length == 0:
+      print("Nothing to pope")
+      return None
+    temp = self.tail
+    if self.length == 1:
+      self.head = None
+      self.tail = None
+    else:
+      self.tail = temp.prev
+      self.tail.next = None
+      temp.prev = None
+    self.length -= 1
+    return temp
+  
+  def prepend(self, value):
+    new_node = NodeD(value)
+    if self.length == 0:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      new_node.next = self.head
+      self.head.prev = new_node
+      self.head = new_node
+    self.length += 1
+    return True
+
+  def pop_first(self):
+    if self.length == 0:
+      print("Nothing to pope")
+      return None
+    temp = self.head
+    if self.length == 1:
+      self.head = None
+      self.tail = None
+    else:
+      self.head = temp.next
+      self.head.prev = None
+      temp.next = None
+    self.length -= 1
+    return temp
+  
+  def get(self, index):
+    # First, handle edge cases
+    if index < 0 or index >= self.length:
+      print("Out of range")
+      return None
+    # Second: iterate over the list, while taking advantage of double link (prev) to make it more efficient
+    temp = self.head
+    if index < self.length / 2:
+      for _ in range(index):
+        temp = temp.next
+    else:
+      temp = self.tail
+      for _ in range(self.length - 1, index, -1):
+        temp = temp.prev
+    
+    return temp
+
+  def set_value(self, index, value):
+    node_to_change = self.get(index)
+
+    if node_to_change:
+      node_to_change.value = value
+      return True
+    
+    return False
+
+  def insert(self, index, value):
+    if index < 0 or index > self.length:
+      return False
+    if index == 0:
+      return self.prepend(value)
+    if index == self.length:
+      return self.append(value)
+    
+    new_node = NodeD(value)
+    before = self.get(index - 1)
+    after = before.next
+    before.next = new_node
+    new_node.prev = before
+    after.prev = new_node
+    new_node.next = after
+
+    self.length += 1
+    return True
+  
+  def remove(self, index):
+    if index < 0 or index >= self.length:
+      return None
+    if index == 0:
+      return self.pop_first()
+    if index == self.length - 1:
+      return self.pop()
+    
+    temp = self.get(index)
+
+    before = temp.prev
+    after = temp.next
+    before.next = after
+    after.prev = before
+    temp.prev = None
+    temp.next = None
+
+    self.length -= 1
+    return temp
+    
+
+     
+
+     
+     
+
+
+
+
+
+
 
 
--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from data_structures.doubly_linked_list import *
 
 def main():
     my_doubly_linked_list = DoublyLinkedList(7)
-    my_doubly_linked_list.append(1)
-    my_doubly_linked_list.append(5)
-
+    my_doubly_linked_list.prepend(8)
+    my_doubly_linked_list.prepend(9)
+    my_doubly_linked_list.insert(1, 5)
     my_doubly_linked_list.print_list()
+    my_doubly_linked_list.remove(1)
+    my_doubly_linked_list.print_list()
+
+  
+
 
 
 if __name__ == "__main__":
